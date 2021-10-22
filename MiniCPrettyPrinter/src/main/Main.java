65833f80099c5aa75250a66c555657d7cc80be88
haeramkeem <ewqdsacxz2345@gmail.com>
2021-10-23 00:59:50 +0900

    init hw3

---
+++ MiniCPrettyPrinter/src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import gen.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        CharStream codeCharStream = CharStreams.fromFileName("test.c");
+        MiniCLexer lexer = new MiniCLexer(codeCharStream);
+        CommonTokenStream tokens = new CommonTokenStream( lexer );
+        MiniCParser parser = new MiniCParser( tokens );
+        ParseTree tree = parser.program();
+        ParseTreeWalker walker = new ParseTreeWalker(); walker.walk(new MiniCPrintListener(), tree);
+    }
+}
