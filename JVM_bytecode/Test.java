5ce1dc5da0b2c9737ddf5a6900c9f71fb37284d3
haeramkeem <ewqdsacxz2345@gmail.com>
2021-11-08 22:56:41 +0900

    add hw04

---
+++ JVM_bytecode/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static int add(int arg1, int arg2) {
+        return arg1 + arg2;
+    }
+
+    public static void main(String[] args) {
+        int tmp = 33;
+        System.out.println(add(tmp, 1));
+    }
+}
